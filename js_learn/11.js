ef40d4819e088dd2737e7054fa659b1039bef8ff
ashok <ak4132592ashok@gmail.com>
2023-03-24 23:41:37 +0530

    falsy and truthy value

--- js_learn/11.js
+++ js_learn/11.js
@@ -1,9 +1,23 @@
 // falsy values
 
-
 // false
 // ""
 //null
 // undefined
 // 0
- 
+
+var fn = "";
+if (fn) {
+  console.log(fn);
+} else {
+  console.log("else");
+}
+
+//truthy
+
+var fn = "Ashok";
+if (fn) {
+  console.log(fn);
+} else {
+  console.log("else");
+}
