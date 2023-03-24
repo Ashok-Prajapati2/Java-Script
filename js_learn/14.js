ac3ec326000464783b6691c40fd5f1f64323b94b
ashok <ak4132592ashok@gmail.com>
2023-03-25 01:04:14 +0530

    switch statement

---
+++ js_learn/14.js
@@ -0,0 +1,29 @@
+// switch statement
+
+var day = 5;
+
+switch (day) {
+  case 0:
+    console.log("sunday");
+  break;
+  case 1:
+    console.log("monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("weneseday");
+    break;
+  case 4:
+    console.log("Thuresday");
+    break;
+  case 5:
+    console.log("friday");
+    break;
+  case 6:
+    console.log("saterday");
+    break;
+    default:
+        console.log("invalid day")
+}
