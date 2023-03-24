c80c1a93db73c193a55032aeaceaa238484b43bd
ashok <ak4132592ashok@gmail.com>
2023-03-24 19:33:57 +0530

    convert string to number // num to str

---
+++ js_learn/08.js
@@ -0,0 +1,67 @@
+// type operator
+// data types(primitive data types)
+//string "ASHOK"
+//number 2 , 152,12,1
+//booleans
+//null
+//BigInt
+// symble
+
+let age = 18
+let fname1 = "ASHOK"
+console.log(typeof fname , typeof age)
+age =  age + fname
+console.log(age)
+console.log(typeof age)
+
+// convert number to string 
+var a = 5100
+a = 5100 + ""
+console.log(typeof(a))
+
+//alternat
+var a = 10
+a = String(a)
+console.log(typeof(a))
+
+
+// convert string to number
+
+var str =  +"ashok"
+console.log(typeof(str))
+
+//altenat 
+
+var a =  "ASHOK"
+a = Number(a)
+console.log(typeof(a))
+
+// string concatenation 
+let fname2 = "ASHOK "
+let lname = "KUMAR "
+let lname1 = "PRAJAPATI"
+let newstr = fname2 + lname + lname1
+console.log(newstr)
+
+// number
+
+var a = 10
+var b = 20
+var c = a + b
+console.log(c)
+
+var a = "10"
+var b = "20"
+c = String(c)
+var c = a+b
+console.log(c)
+
+var a = +"10"
+var b = +"20"
+var c = a+b
+console.log(c)
+
+
+
+
+
