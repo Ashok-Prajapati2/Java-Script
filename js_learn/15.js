0ad74b7f40052a787cd5f16f0c4aa0cce43c2d19
ashok <ak4132592ashok@gmail.com>
2023-03-25 10:32:53 +0530

    // loops // while // for

---
+++ js_learn/15.js
@@ -0,0 +1,36 @@
+//loops
+
+// while loop
+var i = 0;
+
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+
+// sum of first 10 number
+var i = 0;
+var total = 0;
+while (i <= 10) {
+  total = total + i;
+  i++;
+}
+console.log(total);
+
+//
+var n = 10;
+var nsum = (n * (n + 1)) / 2;
+console.log(nsum);
+
+// for lop
+
+for (var i = 1; i <= 10; i++) {
+  console.log(i);
+}
+var total = 0;
+var num = 100;
+for (var i = 0; i <= num; i++) {
+  total = total + i;
+}
+console.log(total);
+
