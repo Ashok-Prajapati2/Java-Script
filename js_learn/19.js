cc49075d72d6490275b6e3556975fcf8fba7d854
ashok <ak4132592ashok@gmail.com>
2023-03-25 19:46:03 +0530

    clone array

--- js_learn/19.js
+++ js_learn/19.js
@@ -20,12 +20,11 @@ var a1 = ["itm1", "itm2"];
 var a2 = [...a1];
 console.log(a1 === a2);
 
-console.log(a1,a2)
-a2.push("itm3")
-console.log(a2, a1)
-
+console.log(a1, a2);
+a2.push("itm3");
+console.log(a2, a1);
 
 //------
 var a1 = ["itm1", "itm2"];
-var a2 = [...a1, "itm3","itm4"];
-console.log(a1 , a2);
+var a2 = [...a1, "itm3", "itm4"];
+console.log(a1, a2);
