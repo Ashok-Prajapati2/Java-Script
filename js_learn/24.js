f264edcba8220fccb6abaddeadc3f7eecc0c971f
ashok <ak4132592ashok@gmail.com>
2023-03-26 18:31:43 +0530

    // object destructing

---
+++ js_learn/24.js
@@ -0,0 +1,51 @@
+// object destructing
+var band = {
+  names: "ASHOK",
+  age: 18,
+  year: 2023,
+};
+var band1 = band.name;
+console.log(band1);
+
+const { names, age, ...rest } = band;
+console.log(names, age, rest);
+
+var { year } = band;
+console.log(band);
+
+//object inside ayyays
+
+const usersinfo = [
+  {
+    userid: 01,
+    name: "ashok",
+    gender: "male",
+  },
+  {
+    userid: 02,
+    name: "kumar",
+    gender: "male",
+  },
+  {
+    userid: 03,
+    name: "prajapati",
+    gender: "male",
+  },
+];
+
+for (var user30 of usersinfo) {
+  console.log(
+    "user data : ",
+    user30.userid,
+    ",",
+    user30.name,
+    ",",
+    user30.gender
+  );
+}
+
+// nested destructuring
+
+const [user1 , user2 , {userid : user}] = usersinfo
+console.log(user1,user2.gender,";",user)
+
