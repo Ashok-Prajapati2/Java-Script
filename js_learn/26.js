42eec0e8066b406eecd8e66fd787cb5d77685f67
ashok <ak4132592ashok@gmail.com>
2023-03-27 19:13:49 +0530

    hoisting

---
+++ js_learn/26.js
@@ -0,0 +1,28 @@
+// hoisting
+hellow1()
+
+function hellow1() {
+    console.log("hellow01")
+}
+
+//////////////////////////////
+
+
+// hellow()
+
+var hellow = () => {
+    console.log("hellow02")
+}
+hellow()
+///////////////////////
+
+// hellow2()
+
+const hellow2 = function() {
+    console.log("hellow03")
+}
+hellow2()
+
+//////////////////
+
+
