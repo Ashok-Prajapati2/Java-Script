aa8bbe88a4cc371426311a0cc8543bdfbc68b273
ashok <ak4132592ashok@gmail.com>
2023-03-27 22:10:53 +0530

    function inside function / lexical scope

--- js_learn/27.js
+++ js_learn/27.js
@@ -15,25 +15,20 @@ var app = () => {
 
 app();
 
-
-
 // lexical scope
 
 const myval = "v1";
 function myapp2() {
-  const myval = "v1";  // comment this  variable than run outside variable myvar
+  const myval = "v1"; // comment this  variable than run outside variable myvar
   var fun = () => {
-    const myval = "v2"  // comment this  variable than run outside variable myvar
-    console.log("inside 10st" , myval);
- app()
-
+    const myval = "v2"; // comment this  variable than run outside variable myvar
+    console.log("inside 10st", myval);
+    app();
   };
 
- const f1 = () => {}
-f1()
+  const f1 = () => {};
+  f1();
   console.log(myval);
 }
 
 myapp2();
-
-
