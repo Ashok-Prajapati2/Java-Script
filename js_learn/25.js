6fa21a825a0f918b1e654ea2c8d28d7a0de07919
ashok <ak4132592ashok@gmail.com>
2023-03-27 18:45:52 +0530

    function

--- js_learn/25.js
+++ js_learn/25.js
@@ -16,33 +16,68 @@ console.log(sum());
 
 // ===============
 
-function sum1(a,b) {
+function sum1(a, b) {
   return a + b;
 }
 console.log(sum1(10, 1));
 
 // ================
 
-  function sumTo(n) {
-    let sum = 0;
-    for (let i = 1; i <= n; i++) {
-      sum += i;
-    }
-    return sum;
+function sumTo(n) {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
   }
-console.log(sumTo(100))
-  
+  return sum;
+}
+console.log(sumTo(100));
 
 //input number
 
-function even(n){
-    if(n%2 == 0){
-        console.log("even")
-    }
-    else{
-       console.log("odd")
-    }
+function even(n) {
+  if (n % 2 == 0) {
+    console.log("even");
+  } else {
+    console.log("odd");
+  }
 }
 
-var n = prompt("number");
-even(n);
+// var n = prompt("number");
+// even(n);
+
+///
+
+function firstc(string) {
+  return string[0];
+}
+
+console.log(firstc("Ashok"));
+
+function find(array, target) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] == target) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+var myarr56 = [1, 2, 3, 5, 9];
+
+var res = find(myarr56, 9);
+console.log(res);
+
+///////function expression
+
+function gmorning() {
+  console.log("Good Morning ASHOK ....... ");
+}
+
+var sin = function () {
+  console.log("Good Morning ASHOK ....... ");
+};
+
+gmorning();
+sin();
+
+////////////////
