7ac8d2ab114b01ce8717050318fe2891d8710b63
ashok <ak4132592ashok@gmail.com>
2023-04-11 00:02:22 +0530

    iterables  , array like object

---
+++ js_learn/35.js
@@ -0,0 +1,36 @@
+//iterables
+//jispe hum for of loop laga sakein
+// string , array are iterable
+
+// array like object
+// jinke pas length property hoti ha
+// aur jiko hum index se access ker sakte hai
+// ex = string
+
+//iterables
+
+var fnamew = "ashok";
+for (let char of fnamew) {
+  console.log(char);
+}
+
+var arraya = [1, 2, 3, 4, 5, 6, 7, "ASHOK"];
+for (let char of arraya) {
+  console.log(char);
+}
+
+// var obj = { id: 1, n: 10 };
+
+// for (let char of obj) {
+//   console.log(char);
+// }
+//  this is not iterables
+
+//=====
+
+// array like object
+
+var ste = "ASHOK"
+console.log(ste.length)
+
+
