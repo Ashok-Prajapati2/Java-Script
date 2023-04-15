63fe36b5ca9fa2e3cd0155893f2049e11a80ebd4
ashok <ak4132592ashok@gmail.com>
2023-04-15 10:38:03 +0530

    change text , change the style  , get and set attrubutes

--- js_learn0/04.js
+++ js_learn0/04.js
@@ -1,2 +1,20 @@
-// change text 
+// change text
 //  textconstent and innerText
+
+// const mainHeading = document.getElementById("main-heading");
+// console.log(mainHeading.textContent) // all text select
+// console.log(mainHeading.innerText) // only unhide text select
+// mainHeading.textContent = "This is text change"; // provide text
+
+
+//change the style 
+// console.log(mainHeading.style);
+// mainHeading.style.backgroundColor = "white";
+// mainHeading.style.color = "red";
+// mainHeading.style.border = "5px solid blue";
+
+//get and set attrubutes
+// const link = document.querySelector("a")
+// console.log(link.getAttribute("href"))
+// link.setAttribute("href" ,"https://google.com")
+// console.log(link.getAttribute("href"))
