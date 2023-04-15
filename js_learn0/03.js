10238267b7786bffabd776cd3466ebfb78c14396
ashok <ak4132592ashok@gmail.com>
2023-04-15 10:11:27 +0530

    selecter in js // id // class // multipal class

---
+++ js_learn0/03.js
@@ -0,0 +1,19 @@
+// select element using get element by id
+
+//console.log(document.getElementById("main-heading"))
+// console.dir(document.getElementById("main-heading"))
+
+//const mainHeading = document.getElementById("main-heading");
+
+//console.log(mainHeading);
+
+// select element using query selector
+
+// const mainHeading = document.querySelector("#main-heading"); // id
+// console.log(mainHeading);
+
+// const header = document.querySelector(".header"); // class
+// console.log(header);
+
+// const navItem = document.querySelectorAll(".nav-item"); // multipal class
+// console.log(navItem);
---
+++ js_learn0/04.js
@@ -0,0 +1,2 @@
+// change text 
+//  textconstent and innerText
