cdb4f21ab23ad040d859b23d455f17e19ee0e8ad
ashok <ak4132592ashok@gmail.com>
2023-04-20 16:02:24 +0530

    childNodes , parentNode ,children

---
+++ js_learn0/05.js
@@ -0,0 +1,32 @@
+// const rootnode = document.getRootNode();
+// console.log(rootnode)
+
+// const htmlnode = rootnode.childNodes[0];
+// console.log(htmlnode)
+
+// const headnode = htmlnode.childNodes[0];
+// console.log(headnode);
+// const textnode = htmlnode.childNodes[1];
+// console.log(textnode);
+// const bodynode = htmlnode.childNodes[2];
+// console.log(bodynode);
+//console.log(headnode.parentNode);
+
+//sibling
+
+// console.log(headnode.nextSibling);
+
+// const h1 = document.querySelector("h1");
+// console.log(h1.firstChild);
+// console.log(h1.parentNode);
+// const div = h1.parentNode.parentNode;
+// div.style.backgroundColor = "blue"
+
+// const body = document.body
+// body.style.backgroundColor = "blue"
+
+// const title = document.title
+// console.log(title)
+
+// const div = document.querySelector("div")
+// console.log(div.children)
