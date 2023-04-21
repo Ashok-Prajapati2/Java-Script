cd6584a9cb503d4288da35793c5f7e5d7dd10b94
ashok <ak4132592ashok@gmail.com>
2023-04-22 00:00:58 +0530

    click

---
+++ js_learn0/08.js
@@ -0,0 +1,36 @@
+// console.log("start");
+const all = document.querySelectorAll(".my-buttons button");
+
+// all.forEach((butten) => {
+//   butten.addEventListener("click", (e) => {
+//     // num = 0;
+//     // for (let i = 0; i < 1000000; i++) {
+//     //   num += i;
+//     // }
+//     // console.log(num)
+//     console.log(e.currentTarget.textContent);
+// });
+// });
+
+// // num = 0;
+// // for (let i = 0; i < 100000000; i++) {
+// //   num += i;
+// // }
+// // console.log(num)
+// // console.log("end");
+
+// function hellow() {
+//   button.style.color = "blue";
+//   button.style.backgroundColor = "red";
+// }
+// for (lls of all) {
+//   lls.addEventListener("click" , hellow);
+// }
+
+// all.forEach((button) => {
+//   button.addEventListener("click", (e) => {
+//     e.target.style.backgroundColor = "red";
+//     e.target.style.color = "blue";
+//   });
+// });
+
