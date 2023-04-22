e11a38f8ee58e08b66b4c46089b11034b95267bd
ashok <ak4132592ashok@gmail.com>
2023-04-22 09:41:46 +0530

    capturing event , // event bubbling / event propogation ,event capturing ,event delegation , event delegation..

--- js_learn0/11.js
+++ js_learn0/11.js
@@ -1,3 +1,53 @@
 // event bubbling / event propogation
 // event capturing
 // event delegation
+
+// const g = document.querySelector(".g")
+// const p = document.querySelector(".p")
+// const c = document.querySelector(".c")
+// const body = document.body;
+
+
+
+// capturing event
+
+// body.addEventListener("click" , ()=>{
+//     console.log("body cp")
+// },true)
+
+// g.addEventListener("click" , ()=>{
+//     console.log("g cp")
+// },true)
+
+// p.addEventListener("click" , ()=>{
+//     console.log("p cp")
+// },true)
+
+// c.addEventListener("click" , ()=>{
+//     console.log("c cp")
+// },true)
+
+
+// //////////
+
+// body.addEventListener("click" , ()=>{
+//     console.log("body")
+// })
+
+// g.addEventListener("click" , ()=>{
+//     console.log("g")
+// })
+// p.addEventListener("click" , ()=>{
+//     console.log("p")
+// })
+// c.addEventListener("click" , ()=>{
+//     console.log("c")
+// })
+
+// event delegation..
+
+// g.addEventListener("click",(e)=>{
+//     console.log(e.target.textContent)
+// })
+
+ 
